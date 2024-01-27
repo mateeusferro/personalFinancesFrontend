30fd74935e77479009833872ab396201b8789ff3
Mateus-Fernandes-Araujo-Ferro <mateus.03fernandesaraujo@gmail.com>
2024-01-27 17:39:51 -0300

    src/app changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,34 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import Provider from "./provider"
+import Link from "next/link"
+import { getServerSession } from "next-auth"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: 'Personal Finances',
-  description: 'This site will take care of your finances',
+  title: "Personal Finances",
+  description: "This site will take care of your finances",
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await getServerSession();
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Provider>
+        <body className={inter.className}>
+          <nav>
+            {!!session && "LOGOUT"}
+            {!session && <Link href="/login">Login</Link>}
+          </nav>
+          {children}
+        </body>
+      </Provider>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+"use client"
+
+import { useSession } from "next-auth/react"
+import Link from "next/link"
+import { useEffect } from "react"
+
 export default function Home() {
+  const { data: session } = useSession()
+  useEffect(() => {
+    console.log(session && session?.user)
+  })
   return (
-    <h1>HEY THERE</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-32">
+      <Link href="/register">REGISTER</Link>
+    </main>
   )
 }
