03e8566767525acdbdaee03e9c3405ee590a7420
Mateus-Fernandes-Araujo-Ferro <mateus.03fernandesaraujo@gmail.com>
2024-01-27 17:43:01 -0300

    adding a content-type header

--- src/services/backend.ts
+++ src/services/backend.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 import { getSession } from "next-auth/react";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 axiosInstance.interceptors.request.use(async (request) => {
