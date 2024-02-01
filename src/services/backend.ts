a5676a393edd991bbe62e6484faefdf64d86f799
Mateus-Fernandes-Araujo-Ferro <mateus.03fernandesaraujo@gmail.com>
2024-02-01 19:08:44 -0300

    service to backend requests with axios

--- src/services/backend.ts
+++ src/services/backend.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { getSession } from "next-auth/react";
 
 const axiosInstance = axios.create({
+  withCredentials: true,
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json"
