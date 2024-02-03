a07bc04f496ae07436a0daaa956d432c31d04f38
Mateus-Fernandes-Araujo-Ferro <mateus.03fernandesaraujo@gmail.com>
2024-02-03 19:19:36 -0300

    new style to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,8 +18,18 @@ export default function Home() {
       {session?.user && <span onClick={() => handleLogout()}>Logout</span>}
       {!session?.user && (
         <>
-          <Link href="/login">Login</Link>
-          <Link href="/register">REGISTER</Link>
+          <Link
+            className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2"
+            href="/login"
+          >
+            Login
+          </Link>
+          <Link
+            className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2"
+            href="/register"
+          >
+            Register
+          </Link>
         </>
       )}
     </main>
